108e8bf971ad7b7d95d38bac02a02e479823e6db
Dr. Lawiy Zodok Shamuel <25419213+Shemsizedek@users.noreply.github.com>
2026-08-19 21:19:50 -0500

    feat(miner): route /miner to NEO MINER ORIGIN

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { MinerApp } from './miner/MinerApp'
 import './styles.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>)
+
+const isMiner = window.location.pathname === '/miner' || window.location.pathname.startsWith('/miner/')
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>{isMiner ? <MinerApp/> : <App/>}</React.StrictMode>
+)
