24c426deca4e2cd8983c275cdf6005c32843521a
Dr. Lawiy Zodok Shamuel <25419213+Shemsizedek@users.noreply.github.com>
2026-08-29 13:39:34 -0500

    chore(omnitrix): bump Android app to v3.9.0

--- apps/omnitrix-android/app/build.gradle.kts
+++ apps/omnitrix-android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "io.neo.omnitrix"
         minSdk = 26
         targetSdk = 35
-        versionCode = 26
-        versionName = "3.8.0"
+        versionCode = 27
+        versionName = "3.9.0"
     }
 
     buildTypes {
