c03863f5f6690862a8bbdc62e8b6fe03d3099901
Dr. Lawiy Zodok Shamuel <25419213+Shemsizedek@users.noreply.github.com>
2026-08-29 14:33:29 -0500

    Release Omnitrix Suit v4.1.0

--- apps/omnitrix-android/app/build.gradle.kts
+++ apps/omnitrix-android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "io.neo.omnitrix"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "4.0.0"
+        versionCode = 29
+        versionName = "4.1.0"
     }
 
     buildTypes {
