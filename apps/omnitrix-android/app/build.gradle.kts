9573e6ac382f501ef0638ff17811c0b282242351
Dr. Lawiy Zodok Shamuel <25419213+Shemsizedek@users.noreply.github.com>
2026-08-29 19:26:08 -0500

    Stamp Omnitrix Suit v4.4.0

--- apps/omnitrix-android/app/build.gradle.kts
+++ apps/omnitrix-android/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "io.neo.omnitrix"
         minSdk = 26
         targetSdk = 35
-        versionCode = 31
-        versionName = "4.3.0"
+        versionCode = 32
+        versionName = "4.4.0"
     }
 
     buildTypes {
