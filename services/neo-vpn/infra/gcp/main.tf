98ea437d39d36b140e0c247ab949ee06b41d5864
Dr. Lawiy Zodok Shamuel <25419213+Shemsizedek@users.noreply.github.com>
2026-08-29 19:35:23 -0500

    NEO VPN hardening: dedicated VPC and runtime identity
    * Harden NEO VPN with dedicated VPC and VM identity
    
    * Replace default VPC input with dedicated gateway subnet
    
    * Update GCP example for dedicated NEO VPN subnet
    
    * Align NEO VPN deploy workflow with dedicated subnet

--- services/neo-vpn/infra/gcp/main.tf
+++ services/neo-vpn/infra/gcp/main.tf
@@ -14,6 +14,27 @@ provider "google" {
   zone    = var.zone
 }
 
+resource "google_compute_network" "neo_vpn" {
+  name                    = "neo-vpn-network"
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+}
+
+resource "google_compute_subnetwork" "neo_vpn_gateway" {
+  name          = "neo-vpn-gateway-subnet"
+  region        = var.region
+  network       = google_compute_network.neo_vpn.id
+  ip_cidr_range = var.gateway_subnet_cidr
+
+  private_ip_google_access = false
+}
+
+resource "google_service_account" "neo_vpn_node_001" {
+  account_id   = "neo-vpn-node-001"
+  display_name = "NEO VPN Node 001"
+  description  = "Dedicated runtime identity for NEO VPN Node 001. No project roles are granted by this module."
+}
+
 resource "google_compute_address" "neo_vpn_node_001" {
   name   = "neo-vpn-node-001-ip"
   region = var.region
@@ -21,7 +42,7 @@ resource "google_compute_address" "neo_vpn_node_001" {
 
 resource "google_compute_firewall" "wireguard" {
   name    = "neo-vpn-wireguard-51820"
-  network = var.network_name
+  network = google_compute_network.neo_vpn.name
 
   direction = "INGRESS"
   priority  = 1000
@@ -31,13 +52,13 @@ resource "google_compute_firewall" "wireguard" {
     ports    = ["51820"]
   }
 
-  source_ranges = var.wireguard_source_ranges
-  target_tags   = ["neo-vpn-gateway"]
+  source_ranges           = var.wireguard_source_ranges
+  target_service_accounts = [google_service_account.neo_vpn_node_001.email]
 }
 
 resource "google_compute_firewall" "iap_ssh" {
   name    = "neo-vpn-iap-ssh"
-  network = var.network_name
+  network = google_compute_network.neo_vpn.name
 
   direction = "INGRESS"
   priority  = 1000
@@ -47,15 +68,14 @@ resource "google_compute_firewall" "iap_ssh" {
     ports    = ["22"]
   }
 
-  source_ranges = ["35.235.240.0/20"]
-  target_tags   = ["neo-vpn-gateway"]
+  source_ranges           = ["35.235.240.0/20"]
+  target_service_accounts = [google_service_account.neo_vpn_node_001.email]
 }
 
 resource "google_compute_instance" "neo_vpn_node_001" {
   name         = "neo-vpn-node-001"
   machine_type = var.machine_type
   zone         = var.zone
-  tags         = ["neo-vpn-gateway"]
 
   can_ip_forward = true
 
@@ -68,7 +88,7 @@ resource "google_compute_instance" "neo_vpn_node_001" {
   }
 
   network_interface {
-    network = var.network_name
+    subnetwork = google_compute_subnetwork.neo_vpn_gateway.id
 
     access_config {
       nat_ip = google_compute_address.neo_vpn_node_001.address
@@ -82,7 +102,8 @@ resource "google_compute_instance" "neo_vpn_node_001" {
   metadata_startup_script = file("${path.module}/startup.sh")
 
   service_account {
-    scopes = ["https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write"]
+    email  = google_service_account.neo_vpn_node_001.email
+    scopes = ["cloud-platform"]
   }
 
   shielded_instance_config {
@@ -99,4 +120,14 @@ resource "google_compute_instance" "neo_vpn_node_001" {
 output "neo_vpn_node_001_public_ip" {
   value       = google_compute_address.neo_vpn_node_001.address
   description = "Public IPv4 endpoint for NEO VPN Node 001."
+}
+
+output "neo_vpn_network_name" {
+  value       = google_compute_network.neo_vpn.name
+  description = "Dedicated VPC used only by NEO VPN infrastructure."
+}
+
+output "neo_vpn_node_001_service_account" {
+  value       = google_service_account.neo_vpn_node_001.email
+  description = "Dedicated runtime service account attached to NEO VPN Node 001."
 }
